9e7a710e637882a3b284360c7cb28b427f3e2b02
ivansomR <ivansom@galileo.edu>
2017-05-23 12:30:25 -0600

    adding navigation functionality and fragments

--- app/src/main/java/com/example/pokocorp/animelistapp/activities/Navigation.java
+++ app/src/main/java/com/example/pokocorp/animelistapp/activities/Navigation.java
@@ -1,4 +1,4 @@
-package com.example.pokocorp.animelistapp;
+package com.example.pokocorp.animelistapp.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.pokocorp.animelistapp.R;
 
 public class Navigation extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
