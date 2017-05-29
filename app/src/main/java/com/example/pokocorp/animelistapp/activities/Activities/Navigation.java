81a69eba84a8af571a2e317df30916b8158f63b8
ivansomR <ivansom@galileo.edu>
2017-05-29 12:06:52 -0600

    adding navigation functionality and fragments

--- app/src/main/java/com/example/pokocorp/animelistapp/activities/Activities/Navigation.java
+++ app/src/main/java/com/example/pokocorp/animelistapp/activities/Activities/Navigation.java
@@ -1,12 +1,15 @@
-package com.example.pokocorp.animelistapp.activities;
+package com.example.pokocorp.animelistapp.activities.Activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +17,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.pokocorp.animelistapp.R;
+import com.example.pokocorp.animelistapp.activities.Fragments.Favorites;
+import com.example.pokocorp.animelistapp.activities.Fragments.Home;
+import com.example.pokocorp.animelistapp.activities.Fragments.Mylist;
 
 public class Navigation extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -42,6 +48,8 @@ public class Navigation extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        showFragment(Home.class);
     }
 
     @Override
@@ -78,22 +86,40 @@ public class Navigation extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
+    @NonNull
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Class fragment;
 
         if (id == R.id.nav_home) {
-            // Handle the camera action
+            fragment = Home.class;
+            showFragment(fragment);
         } else if (id == R.id.nav_mylist) {
-
+            fragment = Mylist.class;
+            showFragment(fragment);
         } else if (id == R.id.nav_favorites) {
-
-        } else if (id == R.id.nav_settings) {
-
+            fragment = Favorites.class;
+            showFragment(fragment);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void showFragment(Class fragmentClass) {
+        Fragment fragment = null;
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.FL_nav, fragment).commit();
     }
 }
