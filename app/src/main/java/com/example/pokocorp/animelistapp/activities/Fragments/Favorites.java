f97f002c949615d86af51011db829db807719e8a
ivansomR <ivansom@galileo.edu>
2017-05-29 13:02:52 -0600

    favorite file added

---
+++ app/src/main/java/com/example/pokocorp/animelistapp/activities/Fragments/Favorites.java
@@ -0,0 +1,30 @@
+package com.example.pokocorp.animelistapp.activities.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pokocorp.animelistapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Favorites extends Fragment {
+
+
+    public Favorites() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorites, container, false);
+    }
+
+}
